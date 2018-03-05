aaff3488bac33b907eda8950c142efd3cc19b67a
Chiin-Git <17188345+Chiin-Git@users.noreply.github.com>
2018-03-05 01:14:50 +0000

    app script

---
+++ EDA_tool/app.R
@@ -0,0 +1,51 @@
+# WiDS Shiny Workshop - complete EDA tool
+
+library(shiny)
+library(ggplot2)
+
+fair_diamonds <- diamonds[diamonds$cut == "Fair", ]
+good_diamonds <- diamonds[diamonds$cut == "Good", ]
+verygood_diamonds <- diamonds[diamonds$cut == "Very Good", ]
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  titlePanel("WiDS Shiny workshop - EDA tool demo"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        inputId = "cut",
+        label = "Select cut of Diamond",
+        choices = c("Fair", "Good", "Very Good"),
+        selected = "Good"
+      )
+    ),
+    mainPanel("Visualisations of Diamond data",
+              tabsetPanel(
+                tabPanel("Price vs Carat", plotOutput(outputId = "scatter")),
+                tabPanel("Volume by Clarity class", plotOutput(outputId = "bar"))
+              )
+    )
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  getDataset <- reactive({
+    switch(input$cut,
+           "Fair" = fair_diamonds,
+           "Good" = good_diamonds,
+           "Very Good" = verygood_diamonds)
+  })
+  
+  output$scatter <- renderPlot({
+    ggplot(getDataset(), aes(price, carat)) + geom_point(aes(colour=color)) + scale_color_brewer(palette="Blues")
+  })
+  
+  output$bar <- renderPlot({
+    ggplot(getDataset(), aes(clarity)) + geom_bar(aes(fill=color)) + scale_fill_brewer(palette="Spectral")
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
